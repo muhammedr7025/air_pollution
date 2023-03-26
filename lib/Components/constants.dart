e6a86c580cf222154e2807de7bd94c66d0c739f5
muhammedr7025 <muhammedr7025@gmail.com>
2023-03-27 01:05:32 +0530

    admi page ui integration

---
+++ lib/Components/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const primaryclr = Colors.blue;
+const secondaryclr = Colors.white;
--- lib/Screens/admin_screen.dart
+++ lib/Screens/admin_screen.dart
@@ -1,11 +1,215 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:air_pollution/Components/constants.dart';
+import 'package:air_pollution/Screens/air_pollution_detail.dart';
+import 'package:air_pollution/Screens/loginscreen.dart';
+import 'package:flutter/material.dart';
 
-class AdminScreen extends StatelessWidget {
-  const AdminScreen({super.key});
+import 'about.dart';
+
+class AdminScreen extends StatefulWidget {
+  const AdminScreen({Key? key}) : super(key: key);
 
   @override
+  State<AdminScreen> createState() => _AdminScreenState();
+}
+
+class _AdminScreenState extends State<AdminScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [
+          const SizedBox(
+            height: 40,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const SizedBox(
+            height: 92,
+            child: CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage(
+                  "https://cdn.vectorstock.com/i/1000x1000/31/95/user-sign-icon-person-symbol-human-avatar-vector-12693195.webp"),
+            ),
+          ),
+          const SizedBox(
+            height: 10, //just for a padding
+          ),
+          const SizedBox(
+            child: Text(
+              "Admin",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 23),
+            ),
+          ),
+          const SizedBox(
+            height: 6, //just for a padding
+          ),
+          const SizedBox(
+            height: 25, //just for a padding
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 10),
+            child: Column(
+              children: [
+                Material(
+                  color: primaryclr,
+                  borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  child: InkWell(
+                    splashColor: secondaryclr,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const About()));
+                    },
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Icon(
+                              Icons.info,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              "About",
+                              style: TextStyle(
+                                  color: Colors.grey[800],
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                            height: 30,
+                            child: Icon(Icons.navigate_next),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Material(
+                  color: primaryclr,
+                  borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  child: InkWell(
+                    splashColor: secondaryclr,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => AirPollutionCausesPage()));
+                    },
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Icon(
+                              Icons.security,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Change Password",
+                              style: TextStyle(
+                                  color: Colors.grey[800],
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                            height: 30,
+                            child: Icon(Icons.navigate_next),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Material(
+                  color: primaryclr,
+                  borderRadius: const BorderRadius.all(Radius.circular(30)),
+                  child: InkWell(
+                    splashColor: secondaryclr,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
+                    onTap: () {
+                      signout(context);
+                    },
+                    child: Container(
+                      height: 50,
+                      padding: const EdgeInsets.all(10),
+                      decoration: const BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(30))),
+                      child: Row(
+                        children: [
+                          SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: Icon(
+                              Icons.logout,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Text(
+                              "Logout",
+                              style: TextStyle(
+                                  color: Colors.grey[800],
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w400),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 30,
+                            height: 30,
+                            child: Icon(Icons.navigate_next),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    ));
+  }
+
+  Future<void> signout(BuildContext ctx) async {
+    Navigator.of(ctx).pushAndRemoveUntil(
+        MaterialPageRoute(builder: (ctx) => LoginScreen()), (route) => false);
   }
 }
