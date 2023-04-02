b74119027babd0ab0908d5e51ef42416a4b9c310
Basith P <basithpofficial@gmail.com>
2023-04-02 13:29:40 +0530

    show pollution data

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
-import 'package:air_pollution/Screens/admin_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import "package:flutter/material.dart";
 
-import 'Screens/login_auth.dart';
+import 'Screens/pollution_data_screen.dart';
 import 'firebase_options.dart';
 
 //import './screens/login_auth.dart';
@@ -20,7 +19,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AdminScreen(),
+      home: PullutionDataScreen(),
     );
   }
 }
